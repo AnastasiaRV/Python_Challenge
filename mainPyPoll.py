9b0e6b8b904c2bfa20d9a2c8fa0b051cb09a0a56
AnastasiaRV <61332049+AnastasiaRV@users.noreply.github.com>
2020-03-31 09:23:57 -0500

    Add files via upload

--- mainPyPoll.py
+++ mainPyPoll.py
@@ -1,6 +1,12 @@
+# Import os module provides a way to use operating system dependent functionality. 
+# The *os* and *os. path* modules include functions to interact with the file system.
+# The csv module implements classes to read and write tabular data in CSV format.
 import os
 import csv
 
+# Breakout the challenge into parts, each using a function to call results 
+# Function header and parameters to pass a csv file, count votes, tabulate winner, and print results to screen and .txt
+# Open csv and save contents to a list named data, skip header 
 def open_read(path):
     with open(path) as data:
         csvreader = csv.reader(data, delimiter = ",")
@@ -10,6 +16,8 @@ def open_read(path):
             data.append(row)
     return (data)
 
+# Pass data from open_read(path) function
+# Create dictionary named poll and store candidate and total_vote data
 def vote_count(data):
     poll = {}
     total_vote = 0
@@ -22,6 +30,7 @@ def vote_count(data):
             poll[candidate] = 1
     return [poll, total_vote]
 
+# Sum up the data, print results for each candidate and show winner
 def tabulate(poll, total_vote):
     vote_count = 0
     winner = ""
@@ -38,13 +47,14 @@ def tabulate(poll, total_vote):
     results = f"""
     Election Results\n
     -------------------------\n
-    Winner: {print_winner} \n
+    Winner: {print_winner}\n
     -------------------------\n
-    Candidates: {print_poll} \n
+    Candidates: {print_poll}\n
     -------------------------\n
     """
     return results
 
+# Define the sequence of called functions to print, export txt  
 def showresults(path):
     vote_csv = open_read(path)
     poll, total_vote = vote_count(vote_csv)
@@ -55,4 +65,5 @@ def showresults(path):
     with open(output_file, 'w') as doc:
         doc.write(results)
 
+# Run the function on the path
 showresults("election_data.csv")
